118c9365a706aea26feb47592c2bbb352cf9569b
andressaurbano <andressa.urbano@hoamtechnology.com>
2024-05-11 08:59:54 -0300

    first commit

---
+++ src/main/java/com/rs/findyourpet/FindYourPetApplication.java
@@ -0,0 +1,13 @@
+package com.rs.findyourpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindYourPetApplication.class, args);
+	}
+
+}
